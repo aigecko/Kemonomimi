fd2fbc6832e1956374270f2b84714f47bb3a92bc
aigecko <aigecko@hotmail.com>
2015-06-06 23:31:26 +0800

    document remake done

--- 規劃.rb
+++ 規劃.rb
@@ -97,7 +97,7 @@ klass={
     震地重擊:周圍敵人受到範圍n+r*matk魔法傷害暈m+s*wis秒#explode
     銳利飛石:前方敵人受到範圍n+r*matk魔法傷害#magic_arrow
 
-    #法杖儲能:增加法杖魔攻n倍的wis#ward_power
+    法杖儲能:增加法杖魔攻n倍的wis#ward_power
 
     大地之力:開啟後增加n%攻速及m%減傷附帶沙塵暴持續r秒#metamorphosis
     沙塵暴:周圍受到範圍n+m*wis傷害並降低r+s*int攻速
@@ -115,7 +115,7 @@ klass={
     寒冰噴吐:前方敵人受到範圍n+m*matk魔傷緩r攻速跑速#magic_arrow
     寒冰凍破:法術命中後造成範圍n+m*int絕對傷害#explode
 
-    #法杖聚焦:增加法杖魔攻n倍的int#ward_power
+    法杖聚焦:增加法杖魔攻n倍的int#ward_power
 
     寒冰之力:開啟後增加n%技吸及m最大hp並降低m%sp消耗持續r秒#metamorphosis
   ],
@@ -126,33 +126,35 @@ klass={
 
     破甲斬擊:普攻造成目標降低n*log(matk)雙防#break_armor
     猛攻之火:增加n+m*matk攻速m跑速持續r秒#boost
-    火焰升溫:對同目標攻擊增加n近攻最多m層#append->solid_defense
-    火圈迸裂:普攻造成範圍n*atk物傷#switch_append->explode
+    烈火戳刺:對同目標攻擊增加n近攻最多m層#append->solid_defense
+    火圈迸裂:第三下普攻造成範圍n*matk魔傷#switch_append->explode
 
     熾焰焚身:每秒造成n+m*matk魔法傷害並降低r%跑速#switch_auto->fire_circle
 
     輕巧雙刀:雙手裝備雙刀時額外增加雙刀攻速xLog(str)的攻速#auto->dual_weapon_atkspd_acc
     再生之盾:增加盾n物防的回復生命以及m魔防的回復法力#auto->rl_weapon
 
-    火焰之力:開啟後增加n%跑速m最大生命且基礎攻速變為r持續s秒#metamorphosis
+    火焰之力:開啟後增加n跑速m%魔法減傷攻擊帶熔火且基礎攻速變為r持續s秒#metamorphosis
+    熔火:範圍n+m*matk無視魔免魔法傷害持續r秒
   ],
   :paladin=>%w[
-    神聖守護:n%機率無視攻擊並增幅m%物防魔防#auto->amplify
+    神聖庇護:n%機率無視攻擊並增加m%雙防#auto->amplify
     聖光反射:受攻擊n%造成範圍m+r*def魔傷及暈眩s秒#attack_defense->counter_beam
-    逆壓之盾:開啟後吸收傷害並在n秒後造成等量物理傷害#pressure_erupt->switch_auto
-    聖光庇佑:魔法免疫n秒並增加m物防魔防#magic_immunuty
-
-    聖光波動:開啟後普攻帶範圍n+m*matk魔傷#smash_wave
-    神聖祝福:開啟後增加n%近攻及m%魔攻持續r秒#boost
+    聖光庇佑:魔法免疫n秒並增加m物防魔防r秒#magic_immunuty
+    
+    輝煌聖光:範圍產生n+m*matk護盾並增加r+s*int攻速
+    聖光波動:普攻帶範圍n+m*atk物理傷害#smash_wave
+    神聖祝福:自身每秒回複n+m*matk生命並增加r+s*atk攻擊持續t秒#recover
 
     一瞬閃光:前方直線n+m*matk魔法傷害#magic_arrow
-    光束打擊:指定範圍n+m*matk魔法傷害並中斷動作#explode
-    自我奉獻:施展法術時範圍增加n+m*int回血回魔持續r秒#attach->contribute
+    光束打擊:指定範圍n+m*matk魔法傷害#explode
+    自我奉獻:施展法術時範圍回復n%最大生命法力#attach->contribute
 
     平衡之盾:增加盾n倍def最大生命及m倍mdef最大法力#auto->rl_weapon
     平衡之槍:增加長槍n倍atk魔攻#auto->single_weapon
 
-    神聖之力:開啟後增加n減傷及m最大生命且基礎攻速變為s持續r秒#metamorphosis
+    神聖之力:開啟後增加n減傷及m最大生命有範圍靈氣且基礎攻速變為s持續r秒#metamorphosis
+    範圍靈氣:跑速n%以及m近攻魔攻
   ],
   :darkknight=>%w[
     血殤靈光:反彈n%絕對傷害給周圍敵人#attack_defense->soul_of_Mototada
@@ -163,38 +165,39 @@ klass={
     真靈切割:普攻n%奪取m%現有生命(s+r*matk)#soul_of_Nagayoshi
     不死戰魂:生命每減少n增加m近攻r物防#soul_of_Nobufusa
 
-    黑暗爆發:範圍n+m*matk絕對傷害緩r%跑速攻速持續s秒(skl_vamp)#explode
+    黑暗爆發:範圍n+m*matk絕對傷害並緩r%跑速攻速持續s秒(skl_vamp)#explode
     黑暗侵蝕:開啟後範圍n+m*maxhp絕對傷害持續r秒(skl_vamp)#soul_of_Kiyomasa
-    虛空重擊:造成n+m*matk絕對傷害中斷動作並提升n%攻速持續r秒(skl_vamp)#soul_of_Yasumasa
+    虛空重擊:造成n+m*matk絕對傷害暈s秒並提升t%攻速持續r秒(skl_vamp)#soul_of_Yasumasa
 
     長槍:paladin
     劍盾:paladin
-
-    黑暗之力:開啟後增加n%輸出傷害及m最大生命且基礎攻速變為r持續s秒#metamorphosis
+    
+    黑暗之力:開啟後增加n%輸出傷害及m最大生命帶負面靈氣且基礎攻速變為r持續s秒#metamorphosis
+    負面靈氣:降低n物防以及m回復效果
   ],
   :crossbowman=>%w[
-    能量轉化:降低受到魔傷n%並增加n弓箭射程#auto->amplify
-    壓電效應:受到傷害獲得n+m*agi跑速r+s*con回血持續t秒#attack_defense->piezoelectric_effect
-    反應裝甲:降低受到的n+m*con普通攻擊#pre_attack_defense->soul_of_Yoshimoto
-
-    震盪輸出:n%機率m倍爆擊並提升r%攻擊速度#auto->amplify
-    深藏不漏:開啟後隱形並提升n%跑速現行造成m額外物理傷害#invisible
+    能量轉化:降低受到魔傷n%並增加m+r*str弓箭射程#auto->amplify
+    巨人外皮:增加r最大生命並降低受到的n+m*con普通攻擊#pre_attack_defense->soul_of_Yoshimoto
+    動如雷霆:開啟後增加n%跑速m韌性
+    
+    積蓄能量:n%機率m倍爆擊並提升r%攻擊速度#auto->amplify
     負電位差:弓箭命中後消減敵方n+m*int法力並造成等量魔法傷害#append->soul_of_Muneshige
-    閃電噴發:開啟後弓箭命中後產生範圍n+m*matk魔法傷害#switch_append->explode
+    閃電噴發:開啟後弓箭命中後產生範圍n+m*agi物理傷害#switch_append->explode
 
     天雷怒擊:指定範圍n+m*matk魔法傷害緩m跑速#explode
     暴怒雷擊:射出讓敵方受到n+m*matk魔法傷害暈眩r秒的弓箭#magic_arrow
 
-    魔法之弩:增加弩n倍ratk魔攻#auto->magic_bow
+    #魔法之弩:增加弩n倍ratk魔攻#auto->magic_bow
 
-    閃電之力:開啟後魔攻遠攻提升n倍並增加m雙防持續r秒#metamorphosis
+    閃電之力:開啟後魔攻遠攻提升n%帶高壓放電並增加m雙防持續r秒#metamorphosis
+    高壓放電:隨機造成n+m*matk魔法傷害
   ],
   :archer=>%w[
-    迅捷之風:降低受到魔傷並增幅閃躲m%#auto->amplify
+    迅捷之風:降低受到n魔傷並增幅閃躲m%#auto->amplify
     身輕如燕:瞬間移動到指定的地點#flash
-    旋風護盾:每n秒自動產生m+r*mdef護盾持續s秒#soul_of_Ittetsu
+    旋風護盾:每n秒自動產生m+r*mdef魔法護盾持續s秒#soul_of_Ittetsu
 
-    疾風祝福:開啟後回復n+m*matk生命並增加m+r*ratk%攻速#boost
+    疾風祝福:開啟後回復n+m*matk生命並增加s+r*ratk%攻速持續t秒#boost
     精準命中:n%機率m倍爆擊並提升r敏捷#auto->amplify
     氣流爆發:n%機率暈m秒額外r傷害#auto->amplify
     無形之箭:弓箭可穿透n敵人並提升弓箭速度m#auto->amplify
@@ -202,8 +205,8 @@ klass={
     望風披靡:射出可以造成n+m*matk魔法傷害並沉默r秒的弓箭#magic_arrow
     狂風之襲:直線造成n+m*matk魔法傷害並降低閃避r%持續s秒#explode
 
-    暴風之弓:弓的攻速強化n倍#magic_bow
+    #暴風之弓:弓的攻速強化n倍#magic_bow
 
-    烈風之力:開啟後隱形並增加n跑速且基礎攻速變為s持續r秒#metamorphosis
+    烈風之力:增加n跑速且基礎攻速變為s持續r秒#metamorphosis
   ]
 }
