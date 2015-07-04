1f20b7cf02f12b0c16ea229688151fbf7778f3f9
aigecko <aigecko@hotmail.com>
2015-07-05 01:29:09 +0800

    update count.rb

--- count.rb
+++ count.rb
@@ -2,7 +2,7 @@
 #coding: utf-8
 line=0
 Dir.foreach('src'){|name|
-  name=~/rb/ and
+  if name=~/\.rb$/
   open("./src/#{name}",'r:utf-8'){|file|
     begin
       line+=file.read.split(/\n/).size
@@ -11,6 +11,21 @@ Dir.foreach('src'){|name|
       exit
     end
   }
+  elsif name=='.'||name=='..'||name=~/\.c$/
+  else
+    path='src/'+name
+    Dir.foreach(path){|name|
+      name=~/\.rb$/ and
+      open("./#{path}/#{name}",'r:utf-8'){|file|
+        begin
+          line+=file.read.split(/\n/).size
+        rescue
+          p name
+          exit
+        end
+      }
+    }
+  end
 }
 open('./main.rbw','r:utf-8'){|file|
   line+=file.read.split(/\n/).size
