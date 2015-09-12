c979f5216108cb47b8f1dbd5cd23ab5de5163aef
AIGecko <aigecko@hotmail.com>
2015-09-13 01:31:29 +0800

    Base::FireCircle remake

--- src/Skill/BaseFireCircle.rb
+++ src/Skill/BaseFireCircle.rb
@@ -1,35 +1,34 @@
 #coding: utf-8
 class Skill::Base::FireCircle
   def self.call(info)
-    attack=info[:args][0]+info[:caster].attrib[:matk]
-    if Game.get_ticks>info[:caster].var[:fire_circle_triger]
-      info[:caster].var[:fire_circle_triger]=Game.get_ticks+1.to_sec
-    else
-      return
-    end
-
+    caster=info[:caster]
+    base_attack,attrib=info[:args]
+    data=info[:data]
+    
+    attack=base_attack+data[:coef]*caster.attrib[data[:coef_sym]]
+    
     Map.add_friend_circle(
-      info[:caster].ally,
+      caster.ally,
       Bullet.new(
-        [Attack.new(info[:caster],type: :mag,cast_type: :skill,attack: attack),
-         Effect.new(info[:caster],
-           name:'燃燒',sym: :circle_burn,effect_type: :slow,
-           icon:'./rc/icon/skill/2011-12-23_3-049.gif',
-           attrib:{wlkspd: info[:args][1]},
-           last: 5.to_sec)],
+        [Attack.new(caster,type: data[:type],cast_type: :skill,attack: attack),
+         Effect.new(caster,
+           name: data[:name],sym: data[:sym],effect_type: data[:effect_type],
+           icon: data[:icon],
+           attrib: attrib,
+           last: data[:last_time].to_sec)],
         Animation.new(:follow,
           {img:['./rc/pic/battle/fire_circle.png'],
             w: 120,h: 60,horizon: true,
             limit: 1
           },
-          [[[:blit,0,24]]]),
+          [[[:blit,0,4]]]),
         :col,
-        caster: info[:caster],
-        x: info[:caster].position.x,
+        caster: caster,
+        x: caster.position.x,
         y: 0,
-        z: info[:caster].position.z,
-        r: 75,
-        h: 50,
+        z: caster.position.z,
+        r: data[:r],
+        h: data[:h],
         live_cycle: :frame,
         surface: :horizon,
         broken_draw: true)
